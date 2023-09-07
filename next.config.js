a88233a198fe9c41cfe17f06bd3ece37ec182b02
jirapong.k.idev <jirapong.k@idev.co.th>
2023-09-07 11:17:15 +0700

    fix: config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
+
+// module.exports = nextConfig
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path')
+
+const nextConfig = {
+   sassOptions: {
+      includePaths: [path.join(__dirname, 'styles')],
+   },
+   experimental: {
+      // outputStandalone: true,
+      serverActions: true,
+   },
+   webpack: (config) => {
+      config.ignoreWarnings = [
+         { module: /node_modules\/node-fetch\/lib\/index\.js/ },
+         { file: /node_modules\/node-fetch\/lib\/index\.js/ },
+      ]
+      return config
+   },
+}
 
 module.exports = nextConfig
