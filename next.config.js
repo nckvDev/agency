74fbc465b5da26372253d9d0d7478baec7611607
jirapong.k.idev <jirapong.k@idev.co.th>
2023-11-30 16:59:57 +0700

    fix: analysis hotjar

--- next.config.js
+++ next.config.js
@@ -1,25 +1,4 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {}
-
-// module.exports = nextConfig
 /** @type {import('next').NextConfig} */
-const path = require('path')
-
-const nextConfig = {
-   sassOptions: {
-      includePaths: [path.join(__dirname, 'styles')],
-   },
-   experimental: {
-      // outputStandalone: true,
-      serverActions: true,
-   },
-   webpack: (config) => {
-      config.ignoreWarnings = [
-         { module: /node_modules\/node-fetch\/lib\/index\.js/ },
-         { file: /node_modules\/node-fetch\/lib\/index\.js/ },
-      ]
-      return config
-   },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
